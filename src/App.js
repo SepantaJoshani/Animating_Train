36956fd15136e554bca0989a0c9f89bfde4a1f8e
Sepanta <Sepanta_97@Yahoo.com>
2021-08-26 02:22:24 +0430

    creating toggling basic red block for working with RTG

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock:false
   };
 
   showModal = () => {
@@ -22,6 +23,12 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button className="Button" onClick={()=>this.setState(prevState=>({
+          showBlock:!prevState.showBlock
+        }))} >Toggle</button>
+        <br />
+        {this.state.showBlock ? <div style={{margin:"auto",backgroundColor:'red',width:100,height:100}}> </div> : null}
+
         {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
         {this.state.modalIsOpen ? <Backdrop how={this.state.modalIsOpen} /> : null}
         <button className="Button" onClick={this.showModal}>
